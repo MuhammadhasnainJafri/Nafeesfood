1e429dfac5d7d1836aa33dfce82e9cab664fbadb
MuhammadhasnainJafri <mhasnainjafriprofessional@gmail.com>
2022-02-02 17:09:26 +0500

    revert head

--- resources/views/Nafees/includes/head.blade.php
+++ resources/views/Nafees/includes/head.blade.php
@@ -1,16 +1,16 @@
 
-<link rel='stylesheet' id='rosa-main-style-css'  href="{{secure_asset('Nafees/Assets/css/style.css' )}} " type='text/css' media='all' />
-<link rel='stylesheet' id='rosa-main-style-css'  href="{{secure_asset('Nafees/Assets/css/header.css')}} " type='text/css' media='all' />
+<link rel='stylesheet' id='rosa-main-style-css'  href="{{asset('Nafees/Assets/css/style.css' )}} " type='text/css' media='all' />
+<link rel='stylesheet' id='rosa-main-style-css'  href="{{asset('Nafees/Assets/css/header.css')}} " type='text/css' media='all' />
 
-<script type='text/javascript' src='{{secure_asset("Nafees/Assets/js/jquery.js")}}'></script>
-<script type='text/javascript' src='{{secure_asset("Nafees/Assets/js/jquery-migrate.min.js")}}'></script>
-<script type='text/javascript' src='{{secure_asset("Nafees/Assets/js/gridable-scripts.js")}}'></script>
-<script type='text/javascript' src='{{secure_asset("Nafees/Assets/js/vendor/modernizr.min.js")}}'></script>
-<script type='text/javascript' src='{{secure_asset("Nafees/Assets/js/webfont.js")}}'></script>
-<script type='text/javascript' src='{{secure_asset("Nafees/Assets/js/TweenMax.min.js")}}'></script>
-<script type='text/javascript' src='{{secure_asset("Nafees/Assets/js/EasePack.min.js")}}'></script>
-<script type='text/javascript' src='{{secure_asset("Nafees/Assets/js/ScrollToPlugin.min.js")}}'></script>
-<script type='text/javascript' src='{{secure_asset("Nafees/Assets/js/index.js")}}'></script>
+<script type='text/javascript' src='{{asset("Nafees/Assets/js/jquery.js")}}'></script>
+<script type='text/javascript' src='{{asset("Nafees/Assets/js/jquery-migrate.min.js")}}'></script>
+<script type='text/javascript' src='{{asset("Nafees/Assets/js/gridable-scripts.js")}}'></script>
+<script type='text/javascript' src='{{asset("Nafees/Assets/js/vendor/modernizr.min.js")}}'></script>
+<script type='text/javascript' src='{{asset("Nafees/Assets/js/webfont.js")}}'></script>
+<script type='text/javascript' src='{{asset("Nafees/Assets/js/TweenMax.min.js")}}'></script>
+<script type='text/javascript' src='{{asset("Nafees/Assets/js/EasePack.min.js")}}'></script>
+<script type='text/javascript' src='{{asset("Nafees/Assets/js/ScrollToPlugin.min.js")}}'></script>
+<script type='text/javascript' src='{{asset("Nafees/Assets/js/index.js")}}'></script>
 <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
 
 <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
